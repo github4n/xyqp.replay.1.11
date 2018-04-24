bd3a418228f92ac18bd3bf8210f35c58e884090d
weixiang.wu <9579249@qq.com>
2018-04-24 09:53:19 +0800

    可以搜索“插入缓存”，即为已插入缓存的地方

---
+++ zhuoan-servers/servers-biz/src/main/java/com/zhuoan/constant/CacheKeyConstant.java
@@ -0,0 +1,23 @@
+package com.zhuoan.constant;
+
+/**
+ * CacheKeyConstant
+ *
+ * @author weixiang.wu
+ * @date 2018 -04-24 09:23
+ */
+public class CacheKeyConstant {
+
+    /**
+     * The constant GAME_SETTING_BY_GID_AND_PLATFORM.房间设置
+     */
+    public static final String GAME_SETTING_BY_GID_AND_PLATFORM = "game_setting_by_gId_and_platform";
+    /**
+     * The constant GAME_INFO_BY_ID.
+     */
+    public static final String GAME_INFO_BY_ID = "game_info_by_id";
+    /**
+     * The constant GAME_SETTING.
+     */
+    public static final String GAME_SETTING = "game_setting";
+}
