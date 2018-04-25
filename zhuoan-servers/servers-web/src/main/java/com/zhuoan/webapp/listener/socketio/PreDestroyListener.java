988ffdfc3c00abf58e8799b9e4fe084551e426f4
weixiang.wu <9579249@qq.com>
2018-04-25 19:28:19 +0800

    日志系统修复

--- zhuoan-servers/servers-web/src/main/java/com/zhuoan/webapp/listener/socketio/PreDestroyListener.java
+++ zhuoan-servers/servers-web/src/main/java/com/zhuoan/webapp/listener/socketio/PreDestroyListener.java
@@ -1,8 +1,12 @@
 package com.zhuoan.webapp.listener.socketio;
 
 import com.zhuoan.service.socketio.SocketIoManagerService;
+import com.zhuoan.util.thread.ThreadPoolHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
@@ -15,8 +19,26 @@ import javax.annotation.Resource;
 @Component
 public class PreDestroyListener {
 
+    private final static Logger logger = LoggerFactory.getLogger(PreDestroyListener.class);
+
     @Resource
     private SocketIoManagerService service;
+
+    /**
+     * Start socket port.
+     */
+    @PostConstruct
+    public void startSocketPort() {
+        if (service.getServer() == null) {
+            logger.info("默认启动socket 服务");
+            ThreadPoolHelper.executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    service.startServer();
+                }
+            });
+        }
+    }
 
     /**
      * Tomcat关闭时，关闭socketio服务端口
