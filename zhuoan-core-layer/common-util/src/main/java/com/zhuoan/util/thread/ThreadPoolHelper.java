df746b88d2739322da5d85b2a13aad5f8f8074d5
weixiang.wu <9579249@qq.com>
2018-04-28 19:17:55 +0800

    队列1-10  Thread 200  task 2

--- zhuoan-core-layer/common-util/src/main/java/com/zhuoan/util/thread/ThreadPoolHelper.java
+++ zhuoan-core-layer/common-util/src/main/java/com/zhuoan/util/thread/ThreadPoolHelper.java
@@ -15,8 +15,8 @@ import java.util.concurrent.TimeUnit;
  **/
 public class ThreadPoolHelper {
 
-    public static  ExecutorService executorService = new ThreadPoolExecutor(8, 24,
+    public static  ExecutorService executorService = new ThreadPoolExecutor(8, 200,
         60L, TimeUnit.SECONDS,
-        new LinkedBlockingQueue<Runnable>(200), new ThreadFactoryBuilder().setNameFormat("call-back-handle-pool-%d").build(), new ThreadPoolExecutor.AbortPolicy());
+        new LinkedBlockingQueue<Runnable>(10), new ThreadFactoryBuilder().setNameFormat("call-back-handle-pool-%d").build(), new ThreadPoolExecutor.AbortPolicy());
 
 }
